f0b493634e583c91f39821356d386be55ee276e9
Darsh4246 <darsh.nayak@outlook.com>
2025-04-13 15:38:37 +0530

    Script.py

--- Script.py
+++ Script.py
@@ -13,12 +13,12 @@ st.markdown("""
     <div style='display: flex; justify-content: space-between; align-items: center;'>
         <h1 style='margin: 0;'>🏏 DRS Ball Tracker</h1>
         <div>
-            <span style='font-size: 0.9rem; color: gray;'>V1.1-Beta</span><br>
+            <span style='font-size: 0.9rem; color: gray;'>V1.1-Alpha</span><br>
             <span style='font-size: 0.75rem; color: gray;'>Changelog: Added trail effect, bounce prediction, speed detection, vertical layout support</span>
         </div>
     </div>
 """, unsafe_allow_html=True)
-st.caption("This is in Beta Phase, so some features may not be fully polished.")
+st.caption("This is in Alpha Phase, so some features may not be fully polished.")
 
 uploaded_file = st.file_uploader("Upload a cricket video", type=["mp4", "mov", "avi"])
 
